201bc7e0cabc0e48f6a6adf5b6c928c4e8abb2d8
bnk5050 <bnkirby@gmail.com>
2018-05-26 16:54:26 -0400

    Update gitignore file.

---
+++ src/librarydb/LibraryDB.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarydb;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brian
+ */
+public class LibraryDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hi Kennedy");
+    Scanner in = new Scanner(System.in);
+    
+    
+        // TODO code application logic here
+    }
+    
+    
+    
+    
+}
